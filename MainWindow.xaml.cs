ea00b1fd6629e458abd6ce859f4eef2ce9cbc3f1
Jakub Medek <medekjak@gmail.com>
2021-06-28 02:16:48 +0200

    Finish to the state of Photomatch_ProofOfConcept (which uses winforms).

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,161 @@ namespace Photomatch_ProofOfConcept_WPF
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private ILogger Logger = null;
+		private List<Perspective> perspectives = new List<Perspective>();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			var multiLogger = new MultiLogger();
+			multiLogger.Loggers.Add(new StatusStripLogger(StatusText));
+			multiLogger.Loggers.Add(new WarningErrorGUILogger());
+			Logger = multiLogger;
+		}
+
+		private void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			string filePath = null;
+			BitmapImage image = null;
+
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Image Files (*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF";
+			openFileDialog.RestoreDirectory = true;
+			if (openFileDialog.ShowDialog() ?? false)
+			{
+				filePath = openFileDialog.FileName;
+			}
+
+			if (filePath == null) // user closed dialog
+			{
+				Logger.Log("Load Image", "User closed dialog before selecting file.", LogType.Info);
+			}
+			else
+			{
+				try
+				{
+					image = new BitmapImage(new Uri(filePath));
+				}
+				catch (Exception ex)
+				{
+					if (ex is FileNotFoundException)
+						Logger.Log("Load Image", "File not found.", LogType.Warning);
+					else if (ex is NotSupportedException)
+						Logger.Log("Load Image", "Incorrect or unsupported image format.", LogType.Warning);
+					else
+						throw ex;
+				}
+			}
+
+			if (image != null)
+			{
+				Logger.Log("Load Image", "File loaded successfully.", LogType.Info);
+
+				var perspective = new Perspective(image);
+				perspectives.Add(perspective);
+				perspective.Apply(MainImage);
+			}
+		}
+	}
+
+	enum LogType
+	{
+		Error, Warning, Info
+	}
+
+	interface ILogger
+	{
+		void Log(string title, string message, LogType type);
+	}
+
+	class MultiLogger : ILogger
+	{
+		public List<ILogger> Loggers { get; } = new List<ILogger>();
+
+		public void Log(string title, string message, LogType type)
+		{
+			foreach (var logger in Loggers)
+			{
+				logger.Log(title, message, type);
+			}
+		}
+	}
+
+	class WarningErrorGUILogger : ILogger
+	{
+		public void Log(string title, string message, LogType type)
+		{
+			switch (type)
+			{
+				case LogType.Error:
+					MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
+					break;
+				case LogType.Warning:
+					MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Warning);
+					break;
+				case LogType.Info:
+					break;
+				default:
+					throw new Exception("Unknown enum type");
+			}
+		}
+	}
+
+	class StatusStripLogger : ILogger
+	{
+		private readonly TextBlock StatusLabel = null;
+		private System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+
+		public StatusStripLogger(TextBlock strip)
+		{
+			StatusLabel = strip;
+
+			timer.Interval = new TimeSpan(hours: 0, minutes: 0, seconds: 5);
+			timer.Tick += Timer_Tick;
+		}
+
+		public void Log(string title, string message, LogType type)
+		{
+			switch (type)
+			{
+				case LogType.Error:
+					message = $"ERROR ({title}): {message}";
+					break;
+				case LogType.Warning:
+					message = $"WARNING ({title}): {message}";
+					break;
+				case LogType.Info:
+					message = $"INFO ({title}): {message}";
+					break;
+				default:
+					throw new Exception("Unknown enum type");
+			}
+
+			StatusLabel.Text = message;
+			timer.Stop();
+			timer.Start();
+		}
+
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			StatusLabel.Text = "";
+			timer.Stop();
+		}
+	}
+
+	class Perspective // should contain image and perspective data
+	{
+		public BitmapImage Image { get; }
+
+		public Perspective(BitmapImage image)
+		{
+			Image = image;
+		}
+
+		public void Apply(Image imageGUI)
+		{
+			imageGUI.Source = Image;
 		}
 	}
 }
