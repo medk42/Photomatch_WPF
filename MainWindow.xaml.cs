9c338d3a8b1ffb18f382ae4f7697544dda824083
Jakub Medek <medekjak@gmail.com>
2021-06-29 18:49:50 +0200

    Add Start Guide and X/Y perspective vector getters

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,6 +31,7 @@ namespace Photomatch_ProofOfConcept_WPF
 		private static readonly double LineEndRadius = 4;
 		private static readonly double LineEndGrabRadius = 8;
 		private static readonly double LineStrokeThickness = 1;
+		private static readonly double StartGuideLineLength = 50;
 
 		public MainWindow()
 		{
@@ -89,14 +90,20 @@ namespace Photomatch_ProofOfConcept_WPF
 				perspectives.Add(perspective);
 				perspective.Apply(MainImage);
 
-				CreateDraggableLine(perspective.LineX1);
-				CreateDraggableLine(perspective.LineY1);
-				CreateDraggableLine(perspective.LineX2);
-				CreateDraggableLine(perspective.LineY2);
+				var startGuide = CreateStartGuide(new Point(image.Width / 2, image.Height / 2));
+
+				var listeners = new List<IChangeListener>();
+				listeners.Add(startGuide);
+
+				CreateDraggableLine(perspective.LineX1, listeners);
+				CreateDraggableLine(perspective.LineY1, listeners);
+				CreateDraggableLine(perspective.LineX2, listeners);
+				CreateDraggableLine(perspective.LineY2, listeners);
+
 			}
 		}
 
-		private void CreateDraggableLine(LineGeometry line)
+		private void CreateDraggableLine(LineGeometry line, List<IChangeListener> changeListeners)
 		{
 			DraggableLineGeometry draggableLine = new DraggableLineGeometry(line, LineEndRadius);
 			geometry.Children.Add(draggableLine.GetGeometry());
@@ -104,6 +111,23 @@ namespace Photomatch_ProofOfConcept_WPF
 			mouseListeners.Add(draggableLineEvents);
 			scalables.Add(draggableLine);
 			scalables.Add(draggableLineEvents);
+
+			foreach (var listener in changeListeners)
+			{
+				draggableLine.AddListener(listener);
+			}
+		}
+
+		private StartGuideGeometry CreateStartGuide(Point point)
+		{
+			StartGuideGeometry startGuide = new StartGuideGeometry(point, perspectives[0], LineEndRadius, StartGuideLineLength);
+			geometry.Children.Add(startGuide.GetGeometry());
+			StartGuideEvents startGuideEvents = new StartGuideEvents(startGuide, LineEndGrabRadius, MainImage);
+			mouseListeners.Add(startGuideEvents);
+			scalables.Add(startGuide);
+			scalables.Add(startGuideEvents);
+
+			return startGuide;
 		}
 
 
@@ -114,6 +138,10 @@ namespace Photomatch_ProofOfConcept_WPF
 
 		private void MainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
 		{
+			Point point = e.GetPosition(MainImage);
+			if (point.X < 0 || point.Y < 0 || point.X >= MainImage.ActualWidth || point.Y >= MainImage.ActualHeight)
+				return;
+
 			foreach (var listener in mouseListeners)
 			{
 				if (listener.MouseDown(sender, e))
@@ -133,6 +161,10 @@ namespace Photomatch_ProofOfConcept_WPF
 
 		private void MainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
 		{
+			Point point = e.GetPosition(MainImage);
+			if (point.X < 0 || point.Y < 0 || point.X >= MainImage.ActualWidth || point.Y >= MainImage.ActualHeight)
+				return;
+
 			foreach (var listener in mouseListeners)
 			{
 				if (listener.MouseUp(sender, e))
@@ -152,6 +184,10 @@ namespace Photomatch_ProofOfConcept_WPF
 
 		private void MainCanvas_MouseMove(object sender, MouseEventArgs e)
 		{
+			Point point = e.GetPosition(MainImage);
+			if (point.X < 0 || point.Y < 0 || point.X >= MainImage.ActualWidth || point.Y >= MainImage.ActualHeight)
+				return;
+
 			foreach (var listener in mouseListeners)
 			{
 				if (listener.MouseMove(sender, e))
@@ -322,6 +358,63 @@ namespace Photomatch_ProofOfConcept_WPF
 		{
 			imageGUI.Source = Image;
 		}
+
+		public Vector GetXVector(Point imagePoint)
+		{
+			Point intersection = GetLineLineIntersection(LineX1.StartPoint, LineX1.EndPoint, LineX2.StartPoint, LineX2.EndPoint);
+			Vector ret = Point.Subtract(intersection, imagePoint);
+			ret.Normalize();
+			return ret;
+		}
+
+		public Vector GetYVector(Point imagePoint)
+		{
+			Point intersection = GetLineLineIntersection(LineY1.StartPoint, LineY1.EndPoint, LineY2.StartPoint, LineY2.EndPoint);
+			Vector ret = Point.Subtract(intersection, imagePoint);
+			ret.Normalize();
+			return ret;
+		}
+
+		public Vector GetZVector(Point imagePoint)
+		{
+			return default;
+			/*Vector xVector = GetXVector(imagePoint);
+			Vector yVector = GetYVector(imagePoint);
+
+			Vector ret = Vector.CrossProduct(xVector, yVector);
+			ret.Normalize();
+			return ret;*/
+		}
+
+		/// <summary>
+		/// Get the point of intersection between the ray and this object using line-line intersection (https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection)
+		/// </summary>
+		/// <param name="StartLineA"></param>
+		/// <param name="EndLineA"></param>
+		/// <param name="StartLineB"></param>
+		/// <param name="EndLineB"></param>
+		/// <returns></returns>
+		private Point GetLineLineIntersection(Point StartLineA, Point EndLineA, Point StartLineB, Point EndLineB)
+		{
+			double x1 = StartLineA.X;
+			double y1 = StartLineA.Y;
+			double x2 = EndLineA.X;
+			double y2 = EndLineA.Y;
+
+			double x3 = StartLineB.X;
+			double y3 = StartLineB.Y;
+			double x4 = EndLineB.X;
+			double y4 = EndLineB.Y;
+
+			double denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+			//if (denominator == 0) return null;
+
+			double t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denominator;
+			double u = ((y1 - y2) * (x1 - x3) - (x1 - x2) * (y1 - y3)) / denominator;
+
+			return new Point(x1 + t * (x2 - x1), y1 + t * (y2 - y1));
+		}
 	}
 
 	interface IScalable
@@ -336,6 +429,7 @@ namespace Photomatch_ProofOfConcept_WPF
 		private EllipseGeometry startPointEllipse;
 		private EllipseGeometry endPointEllipse;
 		private double endRadius;
+		private List<IChangeListener> changeListeners = new List<IChangeListener>();
 
 		public Point StartPoint
 		{
@@ -344,6 +438,7 @@ namespace Photomatch_ProofOfConcept_WPF
 			{
 				line.StartPoint = value;
 				startPointEllipse.Center = value;
+				NotifyListeners();
 			}
 		}
 
@@ -354,7 +449,13 @@ namespace Photomatch_ProofOfConcept_WPF
 			{
 				line.EndPoint = value;
 				endPointEllipse.Center = value;
+				NotifyListeners();
 			}
+		}
+
+		public void AddListener(IChangeListener listener)
+		{
+			changeListeners.Add(listener);
 		}
 
 		public DraggableLineGeometry(LineGeometry line, double endRadius)
@@ -381,6 +482,14 @@ namespace Photomatch_ProofOfConcept_WPF
 			startPointEllipse.RadiusY = endRadius / scale;
 			endPointEllipse.RadiusX = endRadius / scale;
 			endPointEllipse.RadiusY = endRadius / scale;
+		}
+
+		private void NotifyListeners()
+		{
+			foreach (var listener in changeListeners)
+			{
+				listener.NotifyDataChange(this);
+			}
 		}
 	}
 
@@ -472,6 +581,156 @@ namespace Photomatch_ProofOfConcept_WPF
 			if (endOffset.LengthSquared < endMouseRadiusSquared / currentScaleSquared)
 			{
 				draggingEndOffset = endOffset;
+				return true;
+			}
+
+			return false;
+		}
+
+		public void SetNewScale(double scale)
+		{
+			currentScaleSquared = scale * scale;
+		}
+	}
+
+	interface IChangeListener
+	{
+		void NotifyDataChange(object source);
+	}
+
+	class StartGuideGeometry : IScalable, IChangeListener
+	{
+		private GeometryGroup geometry = new GeometryGroup();
+		private LineGeometry lineX = new LineGeometry();
+		private LineGeometry lineY = new LineGeometry();
+		private EllipseGeometry startPointEllipse = new EllipseGeometry();
+		private double endRadius;
+		private double lineLength;
+		private double currentScale = 1;
+		private Perspective perspective;
+
+		private Vector xDirection;
+		private Vector yDirection;
+
+		public Point StartPoint
+		{
+			get => startPointEllipse.Center;
+			set
+			{
+				startPointEllipse.Center = value;
+
+				lineX.StartPoint = value;
+				lineY.StartPoint = value;
+
+				ResetPerspective();
+				SetLines();
+			}
+		}
+
+		public StartGuideGeometry(Point startPoint, Perspective perspective, double endRadius, double lineLength)
+		{
+			this.perspective = perspective;
+			this.endRadius = endRadius;
+			this.lineLength = lineLength;
+
+			this.startPointEllipse.RadiusX = endRadius;
+			this.startPointEllipse.RadiusY = endRadius;
+
+			StartPoint = startPoint;
+
+			geometry.Children.Add(startPointEllipse);
+			geometry.Children.Add(lineX);
+			geometry.Children.Add(lineY);
+		}
+
+		public Geometry GetGeometry()
+		{
+			return geometry;
+		}
+
+		public void SetNewScale(double scale)
+		{
+			startPointEllipse.RadiusX = endRadius / scale;
+			startPointEllipse.RadiusY = endRadius / scale;
+
+			currentScale = scale;
+			SetLines();
+		}
+
+		private void ResetPerspective()
+		{
+			xDirection = perspective.GetXVector(lineX.StartPoint);
+			yDirection = perspective.GetYVector(lineY.StartPoint);
+		}
+
+		private void SetLines()
+		{
+			lineX.EndPoint = Point.Add(lineX.StartPoint, Vector.Multiply(lineLength / currentScale, xDirection));
+			lineY.EndPoint = Point.Add(lineY.StartPoint, Vector.Multiply(lineLength / currentScale, yDirection));
+		}
+
+		public void NotifyDataChange(object source)
+		{
+			if (source.GetType() == typeof(DraggableLineGeometry))
+			{
+				ResetPerspective();
+				SetLines();
+			}
+		}
+	}
+
+	class StartGuideEvents : IMouseEvents, IScalable
+	{
+		private StartGuideGeometry startGuide;
+		private double endMouseRadiusSquared;
+		private double currentScaleSquared = 1;
+		private IInputElement positionRelativeToElement;
+
+		private Vector? draggingOffset = null;
+
+		public StartGuideEvents(StartGuideGeometry startGuide, double endMouseRadius, IInputElement positionRelativeToElement)
+		{
+			this.startGuide = startGuide;
+			this.endMouseRadiusSquared = endMouseRadius * endMouseRadius;
+			this.positionRelativeToElement = positionRelativeToElement;
+		}
+
+		public bool MouseMove(object sender, MouseEventArgs e)
+		{
+			if (draggingOffset != null)
+			{
+				Point eventPos = e.GetPosition(positionRelativeToElement);
+				startGuide.StartPoint = Point.Add(eventPos, draggingOffset.Value);
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool MouseUp(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton != MouseButton.Left)
+				return false;
+
+			if (draggingOffset != null)
+			{
+				draggingOffset = null;
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton != MouseButton.Left)
+				return false;
+
+			Point eventPos = e.GetPosition(positionRelativeToElement);
+			Vector offset = Point.Subtract(startGuide.StartPoint, eventPos);
+			if (offset.LengthSquared < endMouseRadiusSquared / currentScaleSquared)
+			{
+				draggingOffset = offset;
 				return true;
 			}
 
