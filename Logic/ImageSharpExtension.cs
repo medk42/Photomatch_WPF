ec31a88ee5622a25a038ddde7b457d6c65517095
Jakub Medek <medekjak@gmail.com>
2022-02-04 00:30:17 +0100

    Add documentation to ImageSharpExtension.

--- Logic/ImageSharpExtension.cs
+++ Logic/ImageSharpExtension.cs
@@ -5,8 +5,16 @@ using System.Text;
 
 namespace Photomatch_ProofOfConcept_WPF.Logic
 {
+	/// <summary>
+	/// Extension methods for ImageSharp library.
+	/// </summary>
 	public static class ImageSharpExtension
 	{
+		/// <summary>
+		/// Convert color from Rgb24 struct to Vector3.
+		/// </summary>
+		/// <param name="color">color to convert</param>
+		/// <returns>Vector3 with the same values as color (X=R, Y=G, Z=B).</returns>
 		public static Vector3 AsVector3(this Rgb24 color) => new Vector3(color.R, color.G, color.B);
 	}
 }
