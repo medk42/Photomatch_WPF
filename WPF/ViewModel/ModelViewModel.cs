4581845b71beb818a60d16c3efeaf656aff782db
Jakub Medek <medekjak@gmail.com>
2022-02-10 02:57:10 +0100

    Implement face front/back visualization.

--- WPF/ViewModel/ModelViewModel.cs
+++ WPF/ViewModel/ModelViewModel.cs
@@ -45,25 +45,48 @@ namespace Photomatch_ProofOfConcept_WPF.WPF.ViewModel
 
         }
 
+        private Vector3 Translate_;
+        public Vector3 Translate
+        {
+            get => Translate_;
+            set
+			{
+                Translate_ = value;
+                OnPropertyChanged(nameof(Translate));
+            }
+
+        }
+
         public ICommand ViewportLoaded { get; }
 
-        public MeshGeometry3D MeshGeometry { get; } = new MeshGeometry3D();
+        public MeshGeometry3D MeshGeometryFront { get; } = new MeshGeometry3D();
+        public MeshGeometry3D MeshGeometryBack { get; } = new MeshGeometry3D();
 
         private Model Model;
         private Viewport3D Viewport;
-        private Vector3DCollection VertexNormals = new Vector3DCollection();
-        private Point3DCollection VertexPositions = new Point3DCollection();
-        private Int32Collection TriangleIndices = new Int32Collection();
+
+        private Vector3DCollection VertexNormalsFront = new Vector3DCollection();
+        private Point3DCollection VertexPositionsFront = new Point3DCollection();
+        private Int32Collection TriangleIndicesFront = new Int32Collection();
+        private Vector3DCollection VertexNormalsBack = new Vector3DCollection();
+        private Point3DCollection VertexPositionsBack = new Point3DCollection();
+        private Int32Collection TriangleIndicesBack = new Int32Collection();
+
+        private Vector2 DraggingOffset = Vector2.InvalidInstance;
 
         public ModelViewModel(Model model)
 		{
             SetModel(model);
 
-            MeshGeometry.Normals = VertexNormals;
-            MeshGeometry.Positions = VertexPositions;
-            MeshGeometry.TriangleIndices = TriangleIndices;
+            MeshGeometryFront.Normals = VertexNormalsFront;
+            MeshGeometryFront.Positions = VertexPositionsFront;
+            MeshGeometryFront.TriangleIndices = TriangleIndicesFront;
+            MeshGeometryBack.Normals = VertexNormalsBack;
+            MeshGeometryBack.Positions = VertexPositionsBack;
+            MeshGeometryBack.TriangleIndices = TriangleIndicesBack;
 
             ViewportLoaded = new RelayCommand(ViewportLoaded_);
+            Translate = new Vector3(-0.5, 0, 0);
         }
 
         public void ViewportLoaded_(object obj)
@@ -81,42 +104,59 @@ namespace Photomatch_ProofOfConcept_WPF.WPF.ViewModel
 		{
             this.Model = model;
 
-            model.AddFaceEvent += AddFace;
+            model.ModelChangedEvent += ModelChanged;
 
-            foreach (Face face in model.Faces)
+            Recalculate();
+		}
+
+        private void ModelChanged() => Recalculate();
+
+        private void AddTriangle(Triangle triangle, Vector3 normal, bool front)
+		{
+            Vector3DCollection vertexNormals = !front ? VertexNormalsFront : VertexNormalsBack;
+            Point3DCollection vertexPositions = !front ? VertexPositionsFront : VertexPositionsBack;
+            Int32Collection triangleIndices = !front ? TriangleIndicesFront : TriangleIndicesBack;
+
+            vertexNormals.Add(new Vector3D(normal.X, normal.Z, normal.Y));
+            vertexNormals.Add(new Vector3D(normal.X, normal.Z, normal.Y));
+            vertexNormals.Add(new Vector3D(normal.X, normal.Z, normal.Y));
+
+            vertexPositions.Add(new Point3D(triangle.A.Position.X, triangle.A.Position.Z, triangle.A.Position.Y));
+            vertexPositions.Add(new Point3D(triangle.B.Position.X, triangle.B.Position.Z, triangle.B.Position.Y));
+            vertexPositions.Add(new Point3D(triangle.C.Position.X, triangle.C.Position.Z, triangle.C.Position.Y));
+
+            int aIndex = 0 + triangleIndices.Count;
+            int bIndex = 1 + triangleIndices.Count;
+            int cIndex = 2 + triangleIndices.Count;
+
+            triangleIndices.Add(aIndex);
+            triangleIndices.Add(bIndex);
+            triangleIndices.Add(cIndex);
+        }
+
+        private void Recalculate()
+		{
+            VertexNormalsFront.Clear();
+            VertexPositionsFront.Clear();
+            TriangleIndicesFront.Clear();
+            VertexNormalsBack.Clear();
+            VertexPositionsBack.Clear();
+            TriangleIndicesBack.Clear();
+
+            foreach (Face face in Model.Faces)
                 AddFace(face);
-		}
 
-        private void FaceRemove(Face face)
-		{
-            face.FaceRemovedEvent -= FaceRemove;
 
-            UpdateModel(this.Model);
-		}
 
-        private void AddTriangle(Triangle triangle, Vector3 normal)
-		{
-            VertexNormals.Add(new Vector3D(normal.X, normal.Z, normal.Y));
-            VertexNormals.Add(new Vector3D(normal.X, normal.Z, normal.Y));
-            VertexNormals.Add(new Vector3D(normal.X, normal.Z, normal.Y));
+            Vector3 vertexPositionsSum = new Vector3();
+            foreach (Vertex vertex in Model.Vertices)
+                vertexPositionsSum += vertex.Position;
 
-            VertexPositions.Add(new Point3D(triangle.A.Position.X, triangle.A.Position.Z, triangle.A.Position.Y));
-            VertexPositions.Add(new Point3D(triangle.B.Position.X, triangle.B.Position.Z, triangle.B.Position.Y));
-            VertexPositions.Add(new Point3D(triangle.C.Position.X, triangle.C.Position.Z, triangle.C.Position.Y));
-
-            int aIndex = 0 + TriangleIndices.Count;
-            int bIndex = 1 + TriangleIndices.Count;
-            int cIndex = 2 + TriangleIndices.Count;
-
-            TriangleIndices.Add(aIndex);
-            TriangleIndices.Add(bIndex);
-            TriangleIndices.Add(cIndex);
+            Translate = -vertexPositionsSum / Model.Vertices.Count;
         }
 
         private void AddFace(Face face)
 		{
-            face.FaceRemovedEvent += FaceRemove;
-
             Vector3 normal = face.Reversed ? face.Normal : -face.Normal;
 
             foreach (var triangle in face.Triangulated)
@@ -124,19 +164,19 @@ namespace Photomatch_ProofOfConcept_WPF.WPF.ViewModel
                 Triangle reversedTriangle = new Triangle { A = triangle.B, B = triangle.A, C = triangle.C };
                 if (face.Reversed)
 				{
-                    AddTriangle(reversedTriangle, -face.Normal);
-                    AddTriangle(triangle, face.Normal);
+                    AddTriangle(reversedTriangle, -face.Normal, true);
+                    AddTriangle(triangle, face.Normal, false);
 				}
                 else
 				{
-                    AddTriangle(triangle, face.Normal);
-                    AddTriangle(reversedTriangle, -face.Normal);
+                    AddTriangle(triangle, face.Normal, true);
+                    AddTriangle(reversedTriangle, -face.Normal, false);
                 }
 
             }
         }
 
-		public void KeyUp(object sender, KeyEventArgs e)
+        public void KeyUp(object sender, KeyEventArgs e)
 		{
 		}
 
@@ -144,18 +184,31 @@ namespace Photomatch_ProofOfConcept_WPF.WPF.ViewModel
         {
         }
 
+        private Vector2 GetMousePosition(MouseEventArgs e) 
+        {
+            return e.GetPosition(Viewport).AsVector2(); ;
+        }
+
 		public void MouseDown(object sender, MouseButtonEventArgs e)
 		{
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Middle)
+                DraggingOffset = GetMousePosition(e);
 		}
 
 		public void MouseUp(object sender, MouseButtonEventArgs e)
 		{
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Middle)
+                DraggingOffset = Vector2.InvalidInstance;
 		}
 
 		public void MouseMove(object sender, MouseEventArgs e)
 		{
-            Vector2 mouse = e.GetPosition(Viewport).AsVector2();
-            ModelRotate = new Vector3(mouse, 0);
+            if (DraggingOffset.Valid)
+			{
+                Vector2 newOffset = GetMousePosition(e);
+                ModelRotate += new Vector3(newOffset - DraggingOffset, 0);
+                DraggingOffset = newOffset;
+            }
 		}
 
 		public void MouseEnter(object sender, MouseEventArgs e)
@@ -168,16 +221,12 @@ namespace Photomatch_ProofOfConcept_WPF.WPF.ViewModel
 
 		public void MouseWheel(object sender, MouseWheelEventArgs e)
 		{
-            CameraPosition = new Point3D(0, 0, Math.Pow(CameraPosition.Z, 1 + e.Delta / 120.0 / 10));
+            CameraPosition = new Point3D(0, 0, Math.Pow(CameraPosition.Z, 1 - e.Delta / 120.0 / 10));
         }
 
 		public void UpdateModel(Model model)
-		{
-            VertexNormals.Clear();
-            VertexPositions.Clear();
-            TriangleIndices.Clear();
-
-            this.Model.AddFaceEvent -= AddFace;
+        {
+            this.Model.ModelChangedEvent -= ModelChanged;
             SetModel(model);
         }
 	}
